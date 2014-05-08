d57fdadc054a9d571fd7a574bb3dae3bf3eb6e6a
Lunatrius <jadran.kotnik@gmail.com>
2014-05-08 17:31:33 +0200

    Simplified the JSON syntax.

--- src/main/java/com/github/lunatrius/imc/deserializer/NBTTagCompoundDeserializer.java
+++ src/main/java/com/github/lunatrius/imc/deserializer/NBTTagCompoundDeserializer.java
@@ -22,10 +22,9 @@ import net.minecraft.nbt.NBTTagShort;
 import net.minecraft.nbt.NBTTagString;
 
 import java.lang.reflect.Type;
+import java.util.Map;
 
 public class NBTTagCompoundDeserializer implements JsonDeserializer<NBTTagCompound> {
-	public static final String MEMBER_NAME = "name";
-	public static final String MEMBER_VALUE = "value";
 	public static final String DELIMITER = ":";
 
 	public static final String ID_TYPE_BLOCK = "block";
@@ -51,51 +50,60 @@ public class NBTTagCompoundDeserializer implements JsonDeserializer<NBTTagCompou
 		}
 
 		JsonObject jsonObject = json.getAsJsonObject();
-		JsonElement value = jsonObject.get(MEMBER_VALUE);
-		return getCompound(value.getAsJsonArray());
-	}
-
-	private NBTBase getNBTTag(JsonObject jsonObject, String type, String name) {
-		JsonElement value = jsonObject.get(MEMBER_VALUE);
-
-		if (type.equalsIgnoreCase(TYPE_BOOLEAN)) {
-			return new NBTTagByte((byte) (value.getAsBoolean() ? 1 : 0));
-		} else if (type.equalsIgnoreCase(TYPE_BYTE)) {
-			return new NBTTagByte(value.getAsByte());
-		} else if (type.equalsIgnoreCase(TYPE_SHORT)) {
-			NBTBase.NBTPrimitive primitive = getIdFromString(value, type);
-
-			if (primitive != null) {
-				return primitive;
+		for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+			String[] split = entry.getKey().split(DELIMITER, 2);
+			if (split[0].equalsIgnoreCase(TYPE_COMPOUND)) {
+				return getCompound(entry.getValue().getAsJsonArray());
 			}
-
-			return new NBTTagShort(value.getAsShort());
-		} else if (type.equalsIgnoreCase(TYPE_INT)) {
-			NBTBase.NBTPrimitive primitive = getIdFromString(value, type);
-
-			if (primitive != null) {
-				return primitive;
-			}
-
-			return new NBTTagInt(value.getAsInt());
-		} else if (type.equalsIgnoreCase(TYPE_LONG)) {
-			return new NBTTagLong(value.getAsLong());
-		} else if (type.equalsIgnoreCase(TYPE_FLOAT)) {
-			return new NBTTagFloat(value.getAsFloat());
-		} else if (type.equalsIgnoreCase(TYPE_DOUBLE)) {
-			return new NBTTagDouble(value.getAsDouble());
-		} else if (type.equalsIgnoreCase(TYPE_BYTE_ARRAY)) {
-			return getByteArray(value.getAsJsonArray());
-		} else if (type.equalsIgnoreCase(TYPE_STRING)) {
-			return new NBTTagString(value.getAsString());
-		} else if (type.equalsIgnoreCase(TYPE_LIST)) {
-			return getList(value.getAsJsonArray());
-		} else if (type.equalsIgnoreCase(TYPE_COMPOUND)) {
-			return getCompound(value.getAsJsonArray());
-		} else if (type.equalsIgnoreCase(TYPE_INT_ARRAY)) {
-			return getIntArray(value.getAsJsonArray());
 		}
 
+		return null;
+	}
+
+	private NBTBase getNBTTag(JsonObject jsonObject) {
+		for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+			String[] split = entry.getKey().split(DELIMITER, 2);
+			String type = split[0];
+			JsonElement value = entry.getValue();
+
+			if (type.equalsIgnoreCase(TYPE_BOOLEAN)) {
+				return new NBTTagByte((byte) (value.getAsBoolean() ? 1 : 0));
+			} else if (type.equalsIgnoreCase(TYPE_BYTE)) {
+				return new NBTTagByte(value.getAsByte());
+			} else if (type.equalsIgnoreCase(TYPE_SHORT)) {
+				NBTBase.NBTPrimitive primitive = getIdFromString(value, type);
+
+				if (primitive != null) {
+					return primitive;
+				}
+
+				return new NBTTagShort(value.getAsShort());
+			} else if (type.equalsIgnoreCase(TYPE_INT)) {
+				NBTBase.NBTPrimitive primitive = getIdFromString(value, type);
+
+				if (primitive != null) {
+					return primitive;
+				}
+
+				return new NBTTagInt(value.getAsInt());
+			} else if (type.equalsIgnoreCase(TYPE_LONG)) {
+				return new NBTTagLong(value.getAsLong());
+			} else if (type.equalsIgnoreCase(TYPE_FLOAT)) {
+				return new NBTTagFloat(value.getAsFloat());
+			} else if (type.equalsIgnoreCase(TYPE_DOUBLE)) {
+				return new NBTTagDouble(value.getAsDouble());
+			} else if (type.equalsIgnoreCase(TYPE_BYTE_ARRAY)) {
+				return getByteArray(value.getAsJsonArray());
+			} else if (type.equalsIgnoreCase(TYPE_STRING)) {
+				return new NBTTagString(value.getAsString());
+			} else if (type.equalsIgnoreCase(TYPE_LIST)) {
+				return getList(value.getAsJsonArray());
+			} else if (type.equalsIgnoreCase(TYPE_COMPOUND)) {
+				return getCompound(value.getAsJsonArray());
+			} else if (type.equalsIgnoreCase(TYPE_INT_ARRAY)) {
+				return getIntArray(value.getAsJsonArray());
+			}
+		}
 		return null;
 	}
 
@@ -140,14 +148,17 @@ public class NBTTagCompoundDeserializer implements JsonDeserializer<NBTTagCompou
 		for (JsonElement jsonElement : jsonArray) {
 			JsonObject jsonObject = jsonElement.getAsJsonObject();
 
-			String[] split = getSplit(jsonObject);
-			if (split.length != 2) {
-				continue;
-			}
+			for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+				String[] split = entry.getKey().split(DELIMITER, 2);
 
-			NBTBase tag = getNBTTag(jsonObject, split[0], split[1]);
-			if (tag != null) {
-				compound.setTag(split[1], tag);
+				if (split.length != 2) {
+					continue;
+				}
+
+				NBTBase tag = getNBTTag(jsonObject);
+				if (tag != null) {
+					compound.setTag(split[1], tag);
+				}
 			}
 		}
 
@@ -160,22 +171,13 @@ public class NBTTagCompoundDeserializer implements JsonDeserializer<NBTTagCompou
 		for (JsonElement jsonElement : jsonArray) {
 			JsonObject jsonObject = jsonElement.getAsJsonObject();
 
-			String[] split = getSplit(jsonObject);
-			if (split.length != 2) {
-				continue;
-			}
-
-			NBTBase tag = getNBTTag(jsonObject, split[0], split[1]);
+			NBTBase tag = getNBTTag(jsonObject);
 			if (tag != null) {
 				tagList.appendTag(tag);
 			}
 		}
 
 		return tagList;
-	}
-
-	private String[] getSplit(JsonObject jsonObject) {
-		return jsonObject.get(MEMBER_NAME).getAsString().split(DELIMITER, 2);
 	}
 
 	private NBTTagByteArray getByteArray(JsonArray jsonArray) {
