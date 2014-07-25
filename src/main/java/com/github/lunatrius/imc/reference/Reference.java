3b50e99f847e498f711e188b6054aac71dc815cc
Lunatrius <jadran.kotnik@gmail.com>
2014-07-25 02:40:27 +0200

    Added default logger.

--- src/main/java/com/github/lunatrius/imc/reference/Reference.java
+++ src/main/java/com/github/lunatrius/imc/reference/Reference.java
@@ -1,5 +1,6 @@
 package com.github.lunatrius.imc.reference;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Reference {
@@ -9,5 +10,5 @@ public class Reference {
 	public static final String FORGE = "${forgeversion}";
 	public static final String MINECRAFT = "${mcversion}";
 
-	public static Logger logger = null;
+	public static Logger logger = LogManager.getLogger(Reference.MODID);
 }
